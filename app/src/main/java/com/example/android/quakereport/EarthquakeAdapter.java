69d7baf2614750210da92fe7fa3b55526def8036
Marc Grall <mc.grall@hotmail.de>
2017-10-04 00:55:34 +0200

    Format magnitude and display a colored background depending on its height.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,11 +48,25 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake>{
         // find all the textViews within the convertView LinearLayout, and fill them
         // with the info of the current Earthquake object.
 
+
+        // find the TextView that display's the magnitude of the current Earthquake.
+        TextView magnitudeView = convertView.findViewById(R.id.magnitude);
+
         // Format the magnitude, so that it only shows one decimal place.
         String formattedMagnitude = formatMagnitude(current.getMagnitude());
+        // Set its magnitude.
+        magnitudeView.setText(formattedMagnitude);
 
-        TextView magnitude = convertView.findViewById(R.id.magnitude);
-        magnitude.setText(formattedMagnitude);
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(current.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
 
 
 
@@ -105,5 +121,45 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake>{
     private String formatMagnitude(double magnitude) {
         DecimalFormat formatter = new DecimalFormat("0.0");
         return formatter.format(magnitude);
+    }
+
+    private int getMagnitudeColor (double magnitude) {
+        int magnitudeFloor = (int) magnitude;
+        int magnitudeColorResourceId;
+
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 }
